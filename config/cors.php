b0bf2dab9a7d413b49ca1f9ad09a033a430ffec0
SamiraAboutarik <samiraaboutarik45@gmail.com>
2026-03-18 20:54:20 +0000

    fix CORS vercel URL

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths'                    => ['api/*'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://localhost:5173', 'https://jobfinder-frontend-eight.vercel.app'],
+    'allowed_origins'          => ['http://localhost:5173', 'https://jobfinder-dun.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
